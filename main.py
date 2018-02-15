b1bad8ad536a02b07bb59985d6755c1f7ccd1f20
Brian Clee <bpclee2011@gmail.com>
2018-02-14 22:38:51 -0500

    added skeleton python file

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+main.py
+
+@author: Brian Clee
+@date: 2/14/18
+
+@purpose:
+@requirements: Python3.4, pandas, numpy
+@arguments: 
+"""
+
+# imports
+import sys
+import pandas as pd
+import numpy as np
+
+# main
+def main():
+	print("Hello, world.")
+
+main()
