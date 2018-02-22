ca7fbe4d4c18ed6b6bfcac4f2fabb57e9beb3250
Brian Clee <bpclee2011@gmail.com>
2018-02-22 10:58:06 -0500

    documented all the things, and updated readme

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ main.py
 	3) Do we see evidence of Zipf’s law in the summaries
 @requirements: Python3.4, pandas, numpy, nltk (and data, see readme), matplotlib
 @arguments: data/movie_data.csv
-@run: python3 main.py data/movie_data.csv
+@run: from main.py's root -> python3 main.py data/movie_data.csv
+
+@notes: main is located at the bottom of the file, for understanding I'd reccomend reading that first, then
+	move to the top and follow the methods down through the execution path (top_genres, genre_properties, zipfs)
 """
 
 # imports
@@ -69,8 +72,10 @@ def top_genres(data):
 # @purpose: Find out what words are characteristic of the movie summaries in the top genres
 # @param: (sorted_genres) sorted list of genres in descending order of popularity (we only care about the top 5)
 # @param: (data) movie_data.csv as a pandas dataframe object
+#
+# @note: This method is pretty long, for future work breaking this into separate functions would be helpful
 def genre_properties(sorted_genres, data):
-	# create empty genre_tokens dict to hold genre:token lists
+	# create empty genre_tokens dict to hold genre name:token lists
 	genre_tokens = {}
 	# boolean check to see if we have already gone through and tokenized everything
 	files_exist = os.path.isfile("data/top_genres.txt")
@@ -86,7 +91,7 @@ def genre_properties(sorted_genres, data):
 
 		# grab the nltk corpus stopwords
 		stopWords = set(stopwords.words('english'))
-		# add in some extra noise words we don't care about (I definitely missed a couple)
+		# add in some extra noise words we don't care about (I probably missed a couple)
 		noiseWords = ["{{Expand section}}", ",", ".", "(", "[", "{", ")", "]", "}", ":", ";", "&", "'", '"', "'s",
 						"``", "''", "n't", "`", '’']
 
@@ -102,8 +107,10 @@ def genre_properties(sorted_genres, data):
 
 			# don't need to do any trimming on summary strings like we did for genre strings
 			summary_str = str(getattr(row, 'summary'))
+
 			# tokenize the summary string
 			tokens_raw = word_tokenize(summary_str)
+
 			# create an empty token list we will fill with filtered tokens
 			tokens_processed = []
 			# filter the raw token list
@@ -116,7 +123,7 @@ def genre_properties(sorted_genres, data):
 			for genre in genre_str.split(', '):
 				# if the genre is in the top 5 genres genre_tokens dict...
 				if genre in genre_tokens:
-					# then extend the filtered tokens to the end of genre_token's value pair for the current genre
+					# extend the film's filtered tokens to the end of genre_token's token list for the current genre
 					genre_tokens.get(genre).extend(tokens_processed)
 
 		# grab the stop time and alert the user of progress
@@ -125,21 +132,23 @@ def genre_properties(sorted_genres, data):
 
 		# to make sure we never have to do that again, lets store all of our data in some .txt files
 		# first lets store the top 5 genres in the file "top_genres.txt", one genre per line
-		# theoretically this step isn't necessary, but if our corpus changes the top 5 genres could change as well
 		top_genres_file = open("data/top_genres.txt", "w")
 		for i in range(0, 5):
 			# grab the genre name
 			genre = sorted_genres[i]
 			# write it to a line with a newline break
 			top_genres_file.write("%s\n" % genre)
+
 			# using that genre name create a file "genre.txt" where we will store all of that genre's tokens list
 			genre_file = open("data/%s.txt" % genre, "w")
 			# for all of the tokens in that genres value pair from our genre_tokens dict...
 			for token in genre_tokens.get(genre):
 				# write each token on a newline
 				genre_file.write("%s\n" % token)
+
 			# close our genre file inside the for loop since we will use the same variable for all 5 genre files
 			genre_file.close()
+
 		# finally close the top_genres file
 		top_genres_file.close()
 
@@ -151,7 +160,7 @@ def genre_properties(sorted_genres, data):
 
 		# iterate over each line in the file
 		for index, line in enumerate(top_genres_file):
-			# initialize the genre_tokens dict with the top 5 genres as keys, and empty lists for tokesn as values
+			# initialize the genre_tokens dict with the top 5 genres as keys, and empty lists for tokens as values
 			genre_tokens[sorted_genres[index]] = []
 
 		# close the top genres file for memory
@@ -170,19 +179,21 @@ def genre_properties(sorted_genres, data):
 				# append the line (token) to the corresponding token list for the current genre in our genre_tokens dict
 				genre_tokens.get(genre).append(trimmed_line)
 
-			# again close our files
+			# close our files
 			genre_file.close()
 
 		# we are now done loading in our data files and can proceed with addressing the genre characterization
 		print("Done loading!\n")
 
-	# lets create a dict genre_fdicts that will store the genre:freq dist pairs for each genre
+	# at this point, we now have a genre_tokens dict with complete summary tokens lists for each genre
+	# lets create a dict, genre_fdicts, that will store the genre:freq dist pairs for each genre
 	genre_fdists = {}
 	print("Creating frequency distributions for each genre:")
 	# for each of our top genres
 	for genre in genre_tokens.keys():
 		# print out the genre and number of tokens it has
 		print("Total " + str(genre) + " tokens to consider: " + str(len(genre_tokens.get(genre))) + "...")
+
 		# calculate the Frequency Distribution of all the genres tokens
 		fdist = FreqDist(genre_tokens.get(genre))
 		# add the genre:freqdist pair to our genre_fdists dict
@@ -196,6 +207,7 @@ def genre_properties(sorted_genres, data):
 			print(str(genre) + " FreqDist plot does not exist, creating and displaying it now...")
 			# alert the user of how to save the plot so this process is no longer run
 			print("To skip this process in the future, save the figure as `plots/Genre Name_fdist`")
+
 			# plot the top 50 freq dist samples
 			fdist.plot(50, cumulative=True)
 
@@ -205,18 +217,18 @@ def genre_properties(sorted_genres, data):
 	# to begin lets find & store the common set of tokens that is shared between all genre's top 50 freq dist samples
 	common_set = []
 	print("Finding the common set of words across all genre frequency distributions:")
-	# loop through the genres starting with the second (this logic is explained below)
+	# loop through each of the top genres
 	for i in range(0, 5):
-		# grab the current genre from our sorted_genres list that was passed into this function
+		# grab the current genre name from our sorted_genres list that was passed into this function
 		genre = sorted_genres[i]
 		# find the top 50 most common samples in our current genres freq dist
 		top_current = genre_fdists[genre].most_common(50)
+
 		# initialize an empty temporary list that will overwrite our common_set list
 		new_commons = []
 
 		print("Now computing common set additions from " + str(genre) + " genre...")
-
-		# inner loop to compare each genre against every other genre O(n^2), further work could be done to improve this
+		# inner loop to compare each genre against every other genre O(n^2), further work should be done to improve this
 		for j in range(0, 5):
 			# if the genre we are comparing against is the current genre then skip it
 			if j is i:
@@ -226,7 +238,9 @@ def genre_properties(sorted_genres, data):
 			compare_genre = sorted_genres[j]
 			# grab the compare genre's frequency distribution
 			top_compare_raw = genre_fdists[compare_genre].most_common(50)
+
 			# since the most_common function returns a tuple (sample, count) lets strip out just the sample
+			# note: there may be some nltk methods to do this, but I couldnt find any in the documentation
 			top_compare_filtered = []
 			# for all the sample tuples...
 			for sample in top_compare_raw:
@@ -247,23 +261,31 @@ def genre_properties(sorted_genres, data):
 					# then add the sample to common_set
 					common_set.append(sample)
 
+	# we have no computed a comman set of words shared in some combination across the top genres
 	print("A common set has been found! Across all genres " + str(len(common_set)) + " words are shared, they are:")
 	print(common_set)
 
-	# then for each genre list the top unique words
+	# now lets find the unique set of words for each genre, this should give us an insight into genre characteristics
 	print("\nComputing the unique sets for each genre...")
+	# for each genre
 	for genre in genre_tokens.keys():
+		# initialize an empty unique set list
 		unique_set = []
+		# grab the top 50 most common words for the genre from its frequency distribution
 		top_current = genre_fdists[genre].most_common(50)
 
+		# go through each of the top 50 words
 		for sample in top_current:
+			# and if any of the top 50 words arent in the common set...
 			if sample[0] not in common_set:
+				# add it to the unique set
 				unique_set.append(sample[0])
 
+		# finally for each genre print out that genre's name and unique set
 		print(str(genre) + "'s unique set: " + str(unique_set))
 
 # @function: zipfs
-# @purpose: Investigate all of the summaries and see if it exhibits properties of zipfs law
+# @purpose: Investigate all of the summaries and see if the corpus exhibits properties of zipfs law
 # @param: (data) movie_data.csv as a pandas dataframe object
 def zipfs(data):
 	# create empty summary_tokens list to hold tokens from every summary
@@ -290,7 +312,7 @@ def zipfs(data):
 			# tokenize the summary string
 			tokens = word_tokenize(summary_str)
 
-			# check to see if the token is in our noiseTokens list...
+			# check to see if any of the tokens are in our noiseTokens list...
 			for token in tokens:
 				# if its not a noise word...
 				if token not in noiseWords:
@@ -301,7 +323,7 @@ def zipfs(data):
 		t2 = time.time()
 		print("Tokenization completed in " + str(t2 - t1) + " seconds.\n")
 
-		# next lets write our summary tokens to the "summary_tokens.txt" file
+		# next lets write our summary tokens to the "summary_tokens.txt" file so we don't have to do this again
 		summary_file = open("data/summary_tokens.txt", "w")
 		# go through each token in the summary_tokens list
 		for token in summary_tokens:
@@ -336,28 +358,35 @@ def zipfs(data):
 	print("Frequency distribution computed!")
 
 	# caclulate the frequency of our summary_fdist by grabbing the values and sorting descending (plot trends downwards)
+	# this will be our x axis on our zipfs plot
 	freqs = sorted(summary_fdist.values(), reverse=True)
 	# calculate the ranks via our frequencies
+	# this will be our y axis
 	ranks = range(1, len(freqs) + 1)
 
 	print("\nNow plotting the ranks/frequencies of the summary frequency distribution to demonstrate zipfs law...")
 	print("A plot should be displayed shortly, close the plot to finish script execution.")
 
-	# use a loglog plot (log of both axis) from our ranks by freqs
+	# use a loglog plot from matplotlib/pyplot (log of both axis) from our ranks by freqs
 	plt.loglog(ranks, freqs)
+	# label our x axis
 	plt.xlabel('frequency (f)', fontsize=12, fontweight='bold')
+	# label our y axis
 	plt.ylabel('rank (r)', fontsize=12, fontweight='bold')
+	# add a grid for visibility
 	plt.grid(True)
 	# display the plot
 	plt.show()
 
+	# for future work overlaying a red reference line for a true zipfs distribution could be helpful
+
 # @function: main
 # @purpose: driver function that reads in the dataset and calls all other functions
 def main():
-	# check to make sure our dataset was passed in, graceful exit if it wasn't
+	# check to make sure our dataset was passed in, graceful exit (code 1) if it wasn't
 	if len(sys.argv) is not 2:
-		print("Error. Please provide this script with the movie dataset as an argument:")
-		print("python3 main.py movie_data.csv")
+		print("Error. Please provide this script with the movie dataset as an argument like:")
+		print("python3 main.py data/movie_data.csv")
 		sys.exit(1)
 
 	# take the argument and assume its our dataset (this should have more validation checking in the future)
